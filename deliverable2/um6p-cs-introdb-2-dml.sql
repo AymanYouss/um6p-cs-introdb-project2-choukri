21e74a47b106f277ad3a361c5ebd8789dd5792f5
Ayman Youss <youssayman4@gmail.com>
2023-05-10 17:24:26 +0100

    Add DML

--- deliverable2/um6p-cs-introdb-2-dml.sql
+++ deliverable2/um6p-cs-introdb-2-dml.sql
@@ -15,37 +15,34 @@ SELECT B.sid FROM Bill_of_Loading B WHERE  B.bldate='2022-6-23';
 
 SELECT ST.od FROM Sales_Transaction ST WHERE ST.freight_invoice = 116 AND ST.total_volume >= 500;
 
-SELECT D.region FROM Delivery D WHERE D.supplier =’SVN’ AND D.port_loading=’Tanger’;
+SELECT D.region FROM Delivery D WHERE D.supplier ='SVN' AND D.port_loading='Tanger';
 
-SELECT D.contract_status FROM  Delivery D WHERE D.customer_group  LIKE ‘GREEFOS%’;
- —---------------------------------------------------------------------------
+SELECT D.contract_status FROM  Delivery D WHERE D.customer_group  LIKE 'GREEFOS%';
+
+-- --------------------------------------------------------------------------
 #transport id where the weight of product was higher than 80 tons and percentage of ? is equal to 18
 SELECT T.transportid
 FROM Transports T
 JOIN Product P ON T.pid = P.pid
-WHERE P.weight > 80 AND P.percentage = 18
+WHERE P.weight > 80 AND P.percentage = 18;
  
- 
- 
- 
- 
- 
- # contract id of all commands that happened in year 2022 after April where the net quantity was higher than 440
+# contract id of all commands that happened in year 2022 after April where the net quantity was higher than 440
 SELECT C.contract_id
 FROM Bill_of_loading  BL
 JOIN Customer C  ON C.cid = BL.cid
 JOIN Shipment S  ON BL.sid = S.sid
-WHERE  YEAR (S.stuffing_date) = 2022 AND MONTH(S.stuffing_date) > 4 AND S.net_quantity > 440
+WHERE  YEAR (S.stuffing_date) = 2022 AND MONTH(S.stuffing_date) > 4 AND S.net_quantity > 440;
  
  
-Union ?
- 
- #all customers  that ordered  in year 2020 
+#Union ?
+
+
+#all customers  that ordered  in year 2020 
 SELECT BL.cid
 From Bill_of_Loading BL
 INNER JOIN Shipment S
-WHERE S.stuffing_date >= '2020-01-01' AND S.stuffing_date <= ‘2020-12-31’
-ORDER BY stuffing_date
+WHERE S.stuffing_date >= '2020-01-01' AND S.stuffing_date <= '2020-12-31'
+ORDER BY stuffing_date;
  
  
 #number of orders per customer
@@ -63,9 +60,9 @@ JOIN Contains_product CP on CP.pid = P.pid
 JOIN Sales_Transaction ST  on ST.od = CP.od
 WHERE ST.invoice = (	SELECT MAX( ST1.invoice)
 					FROM Sales_Transaction ST1
-				)
+				);
 
-At least six SQL data modification (two update, two deletion, two insertion) commands.
+#At least six SQL data modification (two update, two deletion, two insertion) commands.
 
 #updates
 UPDATE Delivery 
@@ -79,15 +76,17 @@ JOIN Fulfills on Fulfills.od = Records.od
 JOIN Delivery on Delivery.did = Fulfills.did
 SET Delivery.real_freight = 20000 , Customer.contract_status= 'Draft'
 WHERE Fulfills.od= 100 AND Customer.customer_group='CP Group' ;
-—-------------------------------------------------------
+-- -------------------------------------------------------
 #insertion
 
 INSERT INTO Shipment (sid, net_quantity, days_of_storage, days_of_storage2,days_of_storage3, pallets, inspection, loading_date_at_plant, quantity_removed_from_the_site, stuffing_date, sequence_date, storage_cost, storage_cost2, storage_cost3)
-VALUES (1004,1200, 2, 0, 0,’1 pallet per BB ’, 'Inspectorate',’5-9-2023’,110,’5-20-2-2023’,’5-11-2023’,84.91, 66,100);
+VALUES (1004,1200, 2, 0, 0,'1 pallet per BB', 'Inspectorate','5-9-2023',110,'5-20-2023','5-11-2023',84.91, 66,100);
 
 INSERT INTO Sales_transaction (od,freight_invoice, freight_invoice2, freight_invoice3,sales_order, payment_terms, clearance_date,payment_terms_days,incoterm,total_volume,invoice,comment,estimated_fob,payment_status)
-VALUES (105,120, 20.7, 78.4, 1772,’Cable bank transfer ’, '12-25-2022',15,’CIF’,4400,9000,37, 66,’Not paid’);
-—------------------------------------------------------
+VALUES (105,120, 20.7, 78.4, 1772,'Cable bank transfer', '12-25-2022',15,'CIF',4400,9000,37, 66,'Not paid');
+-- ------------------------------------------------------
+
 #Deletion
-DELETE FROM Delivery WHERE status = ‘Finalized’
-DELETE FROM Sales_Transaction WHERE YEAR(tdate) <= 2015
+
+DELETE FROM Delivery WHERE status = 'Finalized';
+DELETE FROM Sales_Transaction WHERE YEAR(tdate) <= 2015;
