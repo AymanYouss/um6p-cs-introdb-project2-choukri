129eac964fba968e165bdc5fb9c0ff2aa7dd6a2d
Ayman Youss <youssayman4@gmail.com>
2023-05-15 17:09:56 +0100

    new table

--- deliverable3/application/src/views/SalesDashboard.php
+++ deliverable3/application/src/views/SalesDashboard.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sales dashboard</title>
-    <link rel="stylesheet" href="../../assets/salesdashboardstyle.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sales dashboard</title>
+  <link rel="stylesheet" href="../../assets/salesdashboardstyle.css">
 </head>
 <body>
+  <div class="container">
     <a class="seesales" href="salesdatabaseview.php">See Sales Database</a>
     <a class="seefull" href="fulldatabaseview.php">See Full Database</a>
-    <a class="modify" href="">Modify rows</a>
-    <a class="add" href="">Add row</a>
+    <a class="modify" href="#">Modify rows</a>
+    <a class="add" href="#">Add row</a>
+  </div>
 </body>
 </html>
