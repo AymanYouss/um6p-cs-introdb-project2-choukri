a598cf3a6881cb4cc32842965f5f169892465cf9
Ayman Youss <youssayman4@gmail.com>
2023-05-19 22:50:50 +0100

    Add style + logout button to pages

--- deliverable3/application/src/views/fulldatabaseview.php
+++ deliverable3/application/src/views/fulldatabaseview.php
@@ -39,13 +39,11 @@
                 <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
                   <ul id="nav" class="navbar-nav ml-auto">
                     <li class="nav-item">
-                      <a class="page-scroll" href="#home">Home</a>
+                      <a class="page-scroll" href="../controllers/redirect.php">Home</a>
                     </li>
+              
                     <li class="nav-item">
-                      <a class="page-scroll" href="#feature">Actions</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="page-scroll" href="#tracking" id="abt" onmouseover="showabt1()" onmouseout="showabt()">About us</a>
+                        <a href="../controllers/logout.php" class="page-scroll">logout</a>
                     </li>
                     
                   </ul>
