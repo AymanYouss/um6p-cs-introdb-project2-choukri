2a50ec0c6930d8667699159acd7c1b4e60d10d9b
maha hanif <maha.hanif@um6p.ma>
2023-05-19 18:12:58 +0100

    style tables and add adv

--- deliverable3/application/src/views/Advdatabaseview.php
+++ deliverable3/application/src/views/Advdatabaseview.php
@@ -7,34 +7,94 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/application/assets/css/tables.css">
+    <link rel="shortcut icon" type="image/x-icon" href="../../assets/img/favicon.svg"/>
+    <link rel="stylesheet" href="../../assets/css/bootstrap-5.0.0-alpha-2.min.css" />
+    <link rel="stylesheet" href="../../assets/css/LineIcons.2.0.css" />
+    <link rel="stylesheet" href="../../assets/css/animate.css" />
+    <link rel="stylesheet" href="../../assets/css/main.css" />
 </head>
-<body>
-<div class="container">
+<header class="header">
+  
+    <!-- Place favicon.ico in the root directory -->
 
-   <div class="">
+    <!-- ========================= CSS here ========================= -->
     
-    <div class="table-responsive">
-      <table class="table table-bordered">
-       <thead>
-        <th>Outbound delivery</th>
-         <th>Avis de chargement status</th>
-         <th>Contract ID</th>
-         <th>Contract Status</th>
-         <th>Invoice</th>
-         <th>Invoiced amount</th>
-         <th>Payment deadline</th>
-         <th>Payment status</th>
-         
-    </thead>
+      <div class="navbar-area">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-12">
+              <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="index.html">
+                  <img src="../../assets/img/logo/lg.webp" style="width:80%" alt="Logo" />
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="toggler-icon"></span>
+                  <span class="toggler-icon"></span>
+                  <span class="toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+                  <ul id="nav" class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                      <a class="page-scroll" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="page-scroll" href="#feature">Actions</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="page-scroll" href="#tracking" id="abt" onmouseover="showabt1()" onmouseout="showabt()">About us</a>
+                    </li>
+                    
+                  </ul>
+                </div>
+                
+                <!-- navbar collapse -->
+              </nav>
+              <!-- navbar -->
+            </div>
+          </div>
+          <!-- row -->
+        </div>
+        <!-- container -->
+      </div>
+      <!-- navbar area -->
+      
+    </header>
+<body>
+<main class="table">
+        <section class="table__header">
+            <h1>Avis de chargement </h1>
+            <div class="input-group">
+                <input type="search" placeholder="Search Data...">
+                
+          
+        </section>
+        <section class="table__body">
+            <table>
+                <thead>
+                    <tr>
+                    <th>Outbound delivery</th>
+                    <th>Avis de chargement status</th>
+                    <th>Contract ID</th>
+                    <th>Contract Status</th>
+                    <th>Invoice</th>
+                    <th>Invoiced amount</th>
+                    <th>Payment deadline</th>
+                    <th>Payment status</th>
+                    </tr>
+                </thead>
+
     <tbody>
+    
     <?php
     //Create new user (just for testing , the user will be in the session array)
     require '../dbconfig.php';
     require '../models/User.php';
     require '../models/PhosphateQueries.php';
-   $userModel = new User($conn);
-   $phosphateUser = new PhosphateQueries($conn);
+    $phosphateUser= new PhosphateQueries($conn);
+    $userModel = new User($conn);
+   
    $tableName = 'temporary_full_table';
   // print_r();
     $fetchData = $userModel->selectADV();
@@ -42,14 +102,24 @@
       foreach($fetchData as $data){
     ?>
       <tr>
-      <td><?php echo $data['od']??''; ?></td>
+      <td><strong><?php echo $data['od']??''; ?></strong></td>
       <td><?php echo $data['ac_status']??''; ?></td>
       <td><?php echo $data['contract_id']??''; ?></td>
       <td><?php echo $data['contract_status']??''; ?></td>
       <td><?php echo $data['invoice']??''; ?></td>
       <td><?php echo $phosphateUser->getInvoicedAmount($data['net_quantity'],$data['total_volume'])??''; ?></td>
       <td><?php echo $phosphateUser->getEta($data['payment_terms_days'],$data['bldate']) ??''; ?></td>
-      <td><?php echo $data['payment_status']??''; ?></td>
+      
+      
+      <?php
+      if (strstr($data['payment_status'],"Paid") && !(strstr($data['payment_status'],"Not")))
+        echo '<td ><p class="status paid">'.$data['payment_status'].'</p></td>';
+      else{
+        echo '<td ><p class="status notpaid">'.$data['payment_status'].'</p></td>';
+
+      }
+      ?>
+      
            
      </tr>
      <?php
@@ -61,12 +131,16 @@
     <tr>
     <?php
     }?>
-    </tbody>
-     </table>
-   </div>
+            </tbody>
+          </table>
+        </section>
+    </main>
+    
+    
+   
  
-</div>
 
-</div>
+
+
 </body>
 </html>
---
+++ deliverable3/application/src/views/editrowADV.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My Form</title>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			background-color: #f2f2f2;
+		}
+
+		.container {
+			max-width: 800px;
+			margin: 0 auto;
+			padding: 20px;
+			background-color: #fff;
+			border-radius: 5px;
+			box-shadow: 0 2px 5px rgba(0,0,0,0.3);
+		}
+
+		h1 {
+			font-size: 24px;
+			text-align: center;
+			margin-bottom: 30px;
+		}
+
+		form {
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+			grid-row-gap: 20px;
+			grid-column-gap: 30px;
+			align-items: center;
+		}
+
+		label {
+			font-weight: bold;
+			margin-bottom: 10px;
+			display: block;
+		}
+
+		input,
+		select {
+			padding: 10px;
+			border-radius: 5px;
+			border: 1px solid #ccc;
+			font-size: 16px;
+			width: 100%;
+			box-sizing: border-box;
+			background-color: #f9f9f9;
+			margin-top: 5px;
+			margin-bottom: 15px;
+		}
+
+		input[type="submit"] {
+			background-color: #4CAF50;
+			color: white;
+			padding: 12px 20px;
+			border: none;
+			border-radius: 5px;
+			cursor: pointer;
+			font-size: 16px;
+			margin-top: 20px;
+		}
+
+		input[type="submit"]:hover {
+			background-color: #3e8e41;
+		}
+
+		@media screen and (max-width: 600px) {
+			form {
+				grid-template-columns: 1fr;
+			}
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<h1>My Form</h1>
+		<form action="../controllers/advEditor.php" method="POST">
+			<label for="od">Outbound delivery:</label>
+			<input type="text" id="od" name="od">
+        
+			
+
+      <input type="submit" value="Submit">
+
+<?php 
+include '../models/User.php';
+?>
