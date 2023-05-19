2d27b090bd1715dc47fad4326d1ccacb507cc435
zakariaCHOUKRI <zc.zakariachoukri23@gmail.com>
2023-05-19 09:27:45 +0100

    Implement logout

--- deliverable3/application/src/views/login_page.php
+++ deliverable3/application/src/views/login_page.php
@@ -59,7 +59,7 @@
 					echo '<div id="error-popup">Incorrect email or password. Please try again.<button id="close-btn">Close</button></div>';
 				} else if ($_GET['error'] == 1)
 				{
-					echo '<div id="error-popup">Email or username already in use.<button id="close-btn">Close</button></div>';
+					echo '<div id="error-popup">Email or username already used. Please try again.<button id="close-btn">Close</button></div>';
 				}
 			}
 		?>
